be6afeb726a3e1fa8746fd0b725d6d534cc1b65f
shubham <tonygupta275@gmail.com>
2023-05-21 20:33:59 +0530

    adding prompt

---
+++ 003-prompt.js
@@ -0,0 +1,10 @@
+const prompt= require('prompt-sync')();
+
+console.log("Example of prompt ");
+//taking input from the user
+ const a=prompt("enter the first number: ");
+ const b=prompt("enter the second number: ");
+//perfroming multipy operator
+ const c= a * b;
+//final result
+ console.log("the addition of a and b is: ",+c);
