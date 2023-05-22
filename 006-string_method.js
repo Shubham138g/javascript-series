5c352a88ca0f098cd9007266f19236d42971a990
shubham <tonygupta275@gmail.com>
2023-05-22 19:55:29 +0530

    added_string_methods

--- 006-string_method.js
+++ 006-string_method.js
@@ -12,3 +12,22 @@ console.log(a.replace("hello","hi"))
 console.log(a)
 
 //substring_method
+console.log(a.substring(3,6))
+
+//trim_method
+let c="        shubham        "
+console.log(c.trim())
+console.log(c)
+
+//UpperCase() Method
+console.log(b.toUpperCase())
+
+
+//lowerCase() Method
+console.log(c.trim().toLocaleLowerCase())
+
+//concat()_method
+console.log(c.trim().toUpperCase().concat(" and it is a concat string").toUpperCase()) //here trim,touppercase and concat string method is used
+
+//charAt()_method
+console.log(c.trim().charAt(5))
