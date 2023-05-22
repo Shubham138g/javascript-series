affb8494cb4c6da64f14d66ae3c8f2e312b81f8d
shubham <tonygupta275@gmail.com>
2023-05-22 19:31:56 +0530

    add_length_method

---
+++ 006-string_method.js
@@ -0,0 +1,8 @@
+//hello world is string "string is collection of character"
+var a="hello world"
+var b= "this is a string"
+console.log(a)
+console.log(b)
+
+//find the length of string.
+console.log(a.length ,b.length)
