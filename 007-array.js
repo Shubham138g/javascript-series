7d9ee1a99c02fd2d780fb81cfe2c2ade3ae8e128
Shubham <100777088+Shubham138g@users.noreply.github.com>
2023-05-23 17:34:51 +0530

    Add files via upload

---
+++ 007-array.js
@@ -0,0 +1,10 @@
+console.log("Array")
+const vichcel=["bmw","ducati","kawasaki"]
+
+//we can print the array in this way
+console.log(vichcel)
+
+//if you want to print the array element one by one then this is an another way to print the array element.
+console.log(vichcel[0])
+console.log(vichcel[1])
+console.log(vichcel[2])
