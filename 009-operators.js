d46d3eec3a62c73d737054fe4cdd9a000f8ef625
Shubham <100777088+Shubham138g@users.noreply.github.com>
2023-05-23 17:36:16 +0530

    Add files via upload

---
+++ 009-operators.js
@@ -0,0 +1,26 @@
+let a=10 // = is a assignment operator
+let b=5
+
+//Arthmetic Operators
+console.log("Arthmetic Operators example")
+console.log(a+b)//addition
+console.log(a-b)//substraction
+console.log(a*b)//multiply
+console.log(a/b)//division
+console.log(a%b)//modolus
+
+//Comparison Operators
+console.log("Comparison Operators example")
+console.log(a<b)//less than
+console.log(a==b)//equal to
+console.log(a>b)//greater than
+console.log(a<=b)//less than equal to
+console.log(a>=b)//greater than equal to
+console.log(a!=b)//not equal
+console.log(a!==b)//not equal value or not equal type
+
+//logical operator
+console.log("logical Operators example")
+console.log(a<12 && b<13)//logical and(&&)
+console.log(a>12 || b>13)//logical or (||)
+console.log(!(a<b))//logical not (!)
