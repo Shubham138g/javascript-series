ac8d46849966d428e624007446160bb3ebb5baf5
shubham <tonygupta275@gmail.com>
2023-05-23 17:17:15 +0530

    inital commit

---
+++ 008-array_method.js
@@ -0,0 +1,24 @@
+console.log("Array")
+const vichcel=["bmw","ducati","kawasaki"]
+
+
+// //length_method
+console.log(vichcel.length)
+
+// //pop_method
+console.log(vichcel.pop())
+console.log(vichcel)
+
+//push_method
+console.log(vichcel.push("hero","honda"))
+console.log(vichcel)
+
+//toString_method
+console.log(vichcel.toString())
+
+//shift_method
+console.log(vichcel.shift())
+console.log(vichcel)
+
+// concat_method
+console.log(vichcel.concat("www","aaa","www"))
