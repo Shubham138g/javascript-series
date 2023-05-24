0f8c1c85932d4185648bd47360094d21bde9e077
shubham <tonygupta275@gmail.com>
2023-05-24 19:47:49 +0530

    add_reduceRight_method

--- 012-array_iteration.js
+++ 012-array_iteration.js
@@ -21,7 +21,18 @@ console.log(x1)
 let num2=[1,67,89,65,6,4,3433]
 
 let x2= num2.reduce(function(a,b){
-    return a+b
+    return a*b
 
 })
 console.log(x2)
+
+
+
+//array reduceright method
+let rr=[1,67,89,65,6,4,3433]
+
+let x5= num2.reduceRight(function(a,b){
+    return a+b
+
+})
+console.log("the sum is ",x5)
