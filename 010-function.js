59f502274badb32f9fb513c9882e0073c8bbb835
shubham <tonygupta275@gmail.com>
2023-05-24 18:36:23 +0530

    inital commit

---
+++ 010-function.js
@@ -0,0 +1,19 @@
+console.log("Function")
+
+
+//function
+function hello(){
+    console.log("hello function")
+}
+
+//call function
+hello()
+
+//second function with return type
+function sum(a,b){
+    return a+b
+}
+
+//calling function
+let a=sum(10,10)
+console.log(a)
