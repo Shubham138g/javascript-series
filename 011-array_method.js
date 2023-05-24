89f0f431d5136354021856af6eb424c13ba442d3
shubham <tonygupta275@gmail.com>
2023-05-24 18:50:05 +0530

    intial commit

---
+++ 011-array_method.js
@@ -0,0 +1,24 @@
+let num=[1,2,3,4,5,6]
+// console.log(num)
+
+//shift method
+// let s= num.shift()
+// console.log(s,num)
+
+//unshift method
+// let s= num.unshift()
+// console.log(s,num)
+
+//reverse_method
+let r= num.reverse()
+console.log(r)
+
+//sort method
+let num1=[222,6,9,1,7,6,]
+let sort= num1.sort()
+console.log(sort) //this sorting is based on alphabetic sorting
+
+//concat method
+let con=num.concat(num1)
+console.log(con)
+console.log(con.sort()) //two array method used in this line first it will concat the array and then sort the araay in alphabetic way
