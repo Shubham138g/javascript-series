dfe74699f79863ec7a64a965572beacb781a3135
shubham <tonygupta275@gmail.com>
2023-05-25 18:41:16 +0530

    intial commit

---
+++ 015-while_loop.js
@@ -0,0 +1,21 @@
+//while loop
+let i=1
+while( i<5){
+    console.log(i)
+    i++
+}
+
+//table using while loop
+let a=1
+let t=2
+while(a<=10){
+    console.log(t*a)
+    a++
+}
+
+//while loop in reverse order
+let i1=5
+while( i1>=1){
+    console.log(i1)
+    i1--
+}
