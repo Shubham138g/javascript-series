ba6257a05d334045c518df1a3774ee7303e19682
shubham <tonygupta275@gmail.com>
2023-05-25 18:30:37 +0530

    intial commit

---
+++ 014-for_loop.js
@@ -0,0 +1,11 @@
+//for loop increment form
+
+for(let a=0;a<10;a++)
+{
+console.log(a)
+}
+
+//for loop in decrement form
+for(let i=10;i>=1;i--){
+    console.log(i)
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-    // console.log(t*i)
+x
