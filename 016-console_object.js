afe9daf01f333038fc49755a08b40f14a10aba66
shubham <tonygupta275@gmail.com>
2023-05-26 19:05:56 +0530

    intial commit

---
+++ 016-console_object.js
@@ -0,0 +1,15 @@
+//console.log()
+console.log("hey! I am console.log")
+
+//console.error()
+console.error("this is an error")
+
+//console.clear()
+
+//console.clear()
+
+//console.assert()
+console.assert(5<1)
+
+//console.info()
+console.info("this is very important information")
