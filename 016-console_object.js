c640118ae4498d0b2c4f979db21c2e52b695d144
shubham <tonygupta275@gmail.com>
2023-05-26 19:11:22 +0530

    add_console.table()

--- 016-console_object.js
+++ 016-console_object.js
@@ -13,3 +13,13 @@ console.assert(5<1)
 
 //console.info()
 console.info("this is very important information")
+
+
+//console.table()
+const data = [
+    { name: 'John', age: 28, city: 'New York' },
+    { name: 'Jane', age: 32, city: 'Chicago' },
+    { name: 'Mike', age: 45, city: 'Los Angeles' }
+  ];
+  
+  console.table(data);
