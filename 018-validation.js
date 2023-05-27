a82bacc37712160c97b53d5b4c90a825b24577ba
shubham <tonygupta275@gmail.com>
2023-05-27 12:21:21 +0530

    add_password_validation

--- 018-validation.js
+++ 018-validation.js
@@ -2,6 +2,7 @@
 function validate_form() {
     let firstname = document.myform.fname.value;
     let pass = document.myform.pass.value;
+    let cpass = document.myform.cpass.value;
 
     if (firstname == null || firstname == "") {
         alert("Name can't be black please enter a name")
@@ -10,6 +11,14 @@ function validate_form() {
     else if(pass.length < 6)
     {
         alert("password at leaet 6 charcter")
+        return false;
+    }
+    else if(pass==cpass)
+    {
+        return true;
+    }
+    else{
+        alert("Password does not match.Please enter coorect password")
         return false;
     }
 
