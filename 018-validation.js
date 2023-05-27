ad3accf701698faaeddd7078a26d89899d4e61f7
shubham <tonygupta275@gmail.com>
2023-05-27 12:09:41 +0530

    add_validation

--- 018-validation.js
+++ 018-validation.js
@@ -1,1 +1,16 @@
 // console.log("hpw are you")
+function validate_form() {
+    let firstname = document.myform.fname.value;
+    let pass = document.myform.pass.value;
+
+    if (firstname == null || firstname == "") {
+        alert("Name can't be black please enter a name")
+        return false;
+    }
+    else if(pass.length < 6)
+    {
+        alert("password at leaet 6 charcter")
+        return false;
+    }
+
+}
