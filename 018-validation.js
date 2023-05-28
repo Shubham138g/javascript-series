904a1c3718e0aa63e55008e2d912dea5752830b9
shubham <tonygupta275@gmail.com>
2023-05-28 19:04:52 +0530

    add_lname_validation

--- 018-validation.js
+++ 018-validation.js
@@ -1,25 +1,17 @@
-// validation for myform form
-
-
-
-
-
-
-
-
 function validate_form() {
 
     //email validation
-    var x=document.myform.email.value;  
-    var atposition=x.indexOf("@");  
-    var dotposition=x.lastIndexOf(".");  
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-      alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
-      return false;  
-      }  
+    var x = document.myform.email.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        alert("Please enter a valid e-mail address \n atpostion:" + atposition + "\n dotposition:" + dotposition);
+        return false;
+    }
 
     //validation for fname.pass,cpass
     let firstname = document.myform.fname.value;
+    let lastname = document.myform.lname.value;
     let pass = document.myform.pass.value;
     let cpass = document.myform.cpass.value;
 
@@ -27,16 +19,18 @@ function validate_form() {
         alert("Name can't be black please enter a name")
         return false;
     }
-    else if(pass.length < 6)
-    {
+    else if (lastname == null || lastname == "") {
+        alert("Name can't be black please enter last name")
+        return false;
+    }
+    else if (pass.length < 6) {
         alert("password at leaet 6 charcter")
         return false;
     }
-    else if(pass==cpass)
-    {
+    else if (pass == cpass) {
         return true;
     }
-    else{
+    else {
         alert("Password does not match.Please enter coorect password")
         return false;
     }
