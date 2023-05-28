0625a3d2a7b37556ad520d4dec09c1265dad28ff
shubham <tonygupta275@gmail.com>
2023-05-28 22:28:04 +0530

    add_if_else

--- 019-if.js
+++ 019-if.js
@@ -1,7 +1,18 @@
 //if statement
 var a=7
-if(a<10)
+var b=7
+if(a<10 || b<12)
 {
-    console.log("a is less the 10")
+    console.log("a and  b is less the 10")
 }
 
+
+
+//if else
+let a1 =12
+if (a1<20){
+    console.log("a1 is less then 20")
+}
+else{
+    console.log("a1 is greater then 20")
+}
