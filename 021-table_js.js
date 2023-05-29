93a7a4cd246e2665fbe3086f089db49cdaa8714f
shubham <tonygupta275@gmail.com>
2023-05-29 20:47:43 +0530

    modify

--- 021-table_js.js
+++ 021-table_js.js
@@ -1,7 +1,8 @@
 function table2() {
     var table1 = document.getElementById("table1").value
+    document.write("<b><center><h2>TABLE OF " +table1 +" IS:</h2></center></b>")
     for (let i = 1; i <= 10; i++) {
-        document.write(table1 + "*" + i + "=" + table1 * i+"<br>")
+        document.write("<b><center>"+table1 + "*" + i + "=" + table1 * i+"</center></b><br>")
     }
 }
 
