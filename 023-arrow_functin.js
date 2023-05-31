37b44b5f8144b994f8287607f9e3fb9c50728fe9
shubham <tonygupta275@gmail.com>
2023-05-31 18:41:47 +0530

    reverse array

--- 023-arrow_functin.js
+++ 023-arrow_functin.js
@@ -7,19 +7,42 @@
 
 const prompt = require('prompt-sync')();
 
-const a = parseInt(prompt("enter first number"))
-const b = parseInt(prompt("enter second number"))
+// const a = parseInt(prompt("enter first number"))
+// const b = parseInt(prompt("enter second number"))
 
-const sum = (a, b) => {
-    let c = a + b;
+// const sum = (a, b) => {
+//     let c = a + b;
 
-    if (c <= 100) {
-        console.log(a, b + " is less than 100");
-        return true;
-    } else {
-        console.log(a, b + " is not less than 100")
-        return false;
-    }
+//     if (c <= 100) {
+//         console.log(a, b + " is less than 100");
+//         return true;
+//     } else {
+//         console.log(a, b + " is not less than 100")
+//         return false;
+//     }
 
+// };
+// console.log(sum(a, b));
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+//function to reverse an array
+
+// const a1=prompt("enter the array element")
+const input = prompt("Enter a list of values separated by commas:");
+
+// Split the input into an array
+const values = input.split(",");
+
+// Display the array
+console.log(values);
+
+const rev=(values) =>{
+      console.log("original array:"+values)
+    //   console.log("array after reversing:" + values.reverse())
+      const r= values.reverse();
+      console.log(r);
+      
 };
-console.log(sum(a, b));
+
+console.log(rev(values));
