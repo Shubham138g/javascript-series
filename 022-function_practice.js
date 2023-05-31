27620d22712479b0836106b6da917072b376c663
shubham <tonygupta275@gmail.com>
2023-05-31 17:53:34 +0530

    add_minute_into_second_func

--- 022-function_practice.js
+++ 022-function_practice.js
@@ -1,15 +1,27 @@
 const prompt= require('prompt-sync')();
 
-let n=prompt("enter the lenght of loop:");
+// let n=prompt("enter the lenght of loop:");
 
-function fizz(n){
-    for(let i=1;i<=n;i++){
-        if(i % 3===0){
-            console.log("fizz")
-        }
-        else(
-            console.log(i)
-        )
-    }
+// function fizz(n){
+//     for(let i=1;i<=n;i++){
+//         if(i % 3===0){
+//             console.log("fizz")
+//         }
+//         else(
+//             console.log(i)
+//         )
+//     }
+// }
+// fizz(n)
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+//convert minute into second
+const number=prompt("enter a number in minute: ");
+
+function convert(number){
+    let s =number*60
+    console.log(s +" second in " +number+" minutes 5")
 }
-fizz(n)
+
+convert(number)
