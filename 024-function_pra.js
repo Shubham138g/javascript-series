55be1e883d1bb58595e1f03d93e00b538e088d67
shubham <tonygupta275@gmail.com>
2023-06-01 20:55:37 +0530

    added_function

--- 024-function_pra.js
+++ 024-function_pra.js
@@ -1,25 +1,45 @@
 const prompt= require('prompt-sync')();
 
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+//take array element from the user and find minimum value and maximun value in the array
+
+// const input = prompt('Enter array elements (comma-separated):');
+// const array = input.split(',');
+// console.log(array);
 
 
-const input = prompt('Enter array elements (comma-separated):');
-const array = input.split(',');
-console.log(array);
+// let minvalue=array[0]
+// let maxvalue=array[0]
 
+// // Printing array elements
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i]<minvalue)
+//     {
+//         minvalue=array[i]
+//     }
+//     if(array[i]>maxvalue){
+//         maxvalue=array[i]
+//     }
 
-let minvalue=array[0]
-let maxvalue=array[0]
+// }
+// console.log("the minmum value in array is :"+minvalue)
+// console.log("the maximum value in array is :"+maxvalue)
 
-// Printing array elements
-for (let i = 0; i < array.length; i++) {
-    if(array[i]<minvalue)
-    {
-        minvalue=array[i]
-    }
-    if(array[i]>maxvalue){
-        maxvalue=array[i]
-    }
+//--------------------------------------------------------------------------------------------------------------------------------------------
 
-}
-console.log("the minmum value in array is :"+minvalue)
-console.log("the maximum value in array is :"+maxvalue)
+//take hour and min from the user and convert them into second and then add both the number and print the second on the console
+
+// const hour=prompt("enter hour:")
+// const min=prompt("enter min:")
+
+// function second(hour,min)
+// {
+//     const a= hour* 3600
+//     const b= min * 60
+//     const c= a + b
+//     console.log(hour+" hours and " + min +" minutes into second is: "+ c +" seconds")
+// }
+// second(hour,min)
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
