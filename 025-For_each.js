3e294d8637ef53e7183825d52480e467c67a1e62
shubham <tonygupta275@gmail.com>
2023-06-02 21:28:50 +0530

    inital commit

--- 025-For_each.js
+++ 025-For_each.js
@@ -1,26 +1,38 @@
-const array=["shubham","mohan","Raju","pri"]
+// const array=["shubham","mohan","Raju","pri"]
 
-array.forEach(element => {
-    console.log(element)
-    
-});
+// array.forEach(element => {
+//     console.log(element)
 
+// });
 
 //print even number using for loop
 
-for(let i=0;i<=100;i++)
-{
-    if(i% 2===0){
-        console.log(i)
-    }
-}
+// for(let i=0;i<=100;i++)
+// {
+//     if(i% 2===0){
+//         console.log(i)
+//     }
+// }
+
+//pattern
+// for(let a=1;a<=10;a++)
+// {
+//     str=""
+//     for(let j=1;j<=a;j++){
+//         str+= j + ""
+//     }
+//     console.log(str)
+// }
+
 
 //pattern
 for(let a=1;a<=10;a++)
 {
     str=""
-    for(let j=1;j<=a;j++){
+    for(let j=0;j<=10-a;j++){
         str+= j + ""
     }
-    console.log(s
+    console.log(str)
 }
+
+
