7d72158a2c27d668b26d0e6f2a249c919d2b8169
shubham <tonygupta275@gmail.com>
2023-06-02 21:17:24 +0530

    even_number_for_loop

--- 025-For_each.js
+++ 025-For_each.js
@@ -4,3 +4,13 @@ array.forEach(element => {
     console.log(element)
     
 });
+
+
+//print even number using for loop
+
+for(let i=0;i<=100;i++)
+{
+    if(i% 2===0){
+        console.log(i)
+    }
+}
