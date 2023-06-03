a73f5b8ffbe8904223eb5f70c16bcbbac89c313b
shubham <tonygupta275@gmail.com>
2023-06-03 19:06:32 +0530

    intial commit

--- 027-practice.js
+++ 027-practice.js
@@ -20,11 +20,21 @@ const prompt= require('prompt-sync')();
 
 
 
-let c1=" how are you"
-const c=prompt("enter your name")
-const concat =(c) =>{
-    console.log(c.concat(c1))
+// let c1=" how are you"
+// const c=prompt("enter your name")
+// const concat =(c) =>{
+//     console.log(c.concat(c1))
     
-}
+// }
 
-concat(c)
+// concat(c)
+
+
+//array
+const arr=[1,2,3,4,5,6,7,8,9]
+
+console.log(arr.length)
+console.log(arr.pop())
+console.log(arr)
+console.log(arr.push(12))
+console.log(arr)
