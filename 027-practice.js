2c23b9865e918b926f1a9e5428a9aa44c1559173
shubham <tonygupta275@gmail.com>
2023-06-03 18:52:46 +0530

    uper

--- 027-practice.js
+++ 027-practice.js
@@ -1,10 +1,21 @@
 const prompt= require('prompt-sync')();
 
-const a=prompt("enter the number")
+// const a=prompt("enter the number")
 
-function lower(a)
-{
-    console.log(a.toLowerCase())
+// function lower(a)
+// {
+//     console.log(a.toLowerCase())
 
+// }
+// lower(a)
+
+
+//arrow function
+const b=prompt("enter a name")
+const uper =(b) =>{
+    console.log(b.toUpperCase())
 }
-lower(a)
+
+uper(b)
+
+
