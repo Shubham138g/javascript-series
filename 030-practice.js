df1b8e4443a9de1c7efad53c6907b781d2e29321
shubham <tonygupta275@gmail.com>
2023-06-14 18:32:39 +0530

    javascript instance of a  object

--- 030-practice.js
+++ 030-practice.js
@@ -1,3 +1,14 @@
 //Javascript Object
-emp={id:206040,name:"shubham",adderess:"Janak Nagar",city:"Jalandhar",State:"Punjab"}
-console.log(emp.city)
+// emp={id:206040,name:"shubham",adderess:"Janak Nagar",city:"Jalandhar",State:"Punjab"}
+// console.log(emp.city)
+
+//instance of a object
+
+let emp1=new Object();
+emp1.salary=15000;
+emp1.dp="Computer Science";
+emp1.city="Jalandhar";
+emp1.state="Punjab";
+
+console.log(emp1.salary)
+
