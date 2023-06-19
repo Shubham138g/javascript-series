a8a2b8c34a8f387bd809a85c2c9019c1800df0f6
shubham <tonygupta275@gmail.com>
2023-06-19 21:35:33 +0530

    try-catch-finally

--- 32-exception-handling.js
+++ 32-exception-handling.js
@@ -1,9 +1,22 @@
-try {
-    let a=10;
-    console.log(`the value of a is ${a}`);
-    console.log(b);
+// try {
+//     let a=10;
+//     console.log(`the value of a is ${a}`);
+//     console.log(b);
 
     
+// } catch (error) {
+//     console.log(error)
+// }
+
+
+try {
+    let a=2
+    if(a==2){
+        console.log(`ok`)
+    }
 } catch (error) {
-    console.log(error)
+    console.log(error)   
+}
+finally{
+    console.log("a is eq to 2")
 }
